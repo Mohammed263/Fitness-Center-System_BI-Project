7129f948e2fbe70a6037667f491d85bb2bea00df
Mohammad Anwar <mohalema263@gmail.com>
2025-11-23 14:35:40 +0200

    SSRS Queries

--- Queries/SSRS Reports_SPs.sql
+++ Queries/SSRS Reports_SPs.sql
@@ -1,5 +1,5 @@
 ﻿
-go
+-- Trainer_Salaries
 
 create or alter proc Trainer_Salaries @Branch nvarchar(max)
 as
@@ -16,6 +16,8 @@ end
 
 go
 
+-- Staff_Salaries
+
 create or alter proc Staff_Salaries @Branch nvarchar(max)
 as
 begin
@@ -28,6 +30,8 @@ where b.ID in (SELECT CAST(value AS INT)
 end
 
 go
+
+-- Equipment_Maintenace
 
 create or alter proc Equipment_Maintenace @Branch nvarchar(max)
 as
@@ -46,6 +50,8 @@ end
 
 go
 
+-- Supplies_Details
+
 create or alter proc Supplies_Details @Branch nvarchar(max)
 as
 begin
@@ -61,93 +67,7 @@ end
 
 go
 
-create or alter proc RFM
-as
-begin
-with rfm_base as(
-	select
-		TraineeID,
-		BranchID,
-		max(cast(Date as datetime)) as 'Last_Order_Date',
-		count(distinct InvoiceNumber) as 'Invoice_Count',
-		sum(CAST(TotalAmount as decimal(18,2))) as 'Total_Price'
-	from PaymentDetails
-	group by TraineeID,BranchID
-),
-
-rfm as(
-	select 
-		TraineeID,
-		BranchID,
-		DATEDIFF(DAY, Last_Order_Date, GETDATE()) as 'Recency',
-		Invoice_Count,
-		Total_Price
-	from rfm_base
-),
-
--- STEP 1: Get distinct values only
-freq_raw as (
-    select distinct Invoice_Count
-    from rfm
-),
-
--- STEP 2: Rank values from highest to lowest
-freq_ranked as (
-    select
-        Invoice_Count,
-        DENSE_RANK() over (order by Invoice_Count asc) as rank_desc
-    from freq_raw
-),
-
--- STEP 3: Convert rank → 5 buckets (exactly like NTILE but correct)
-frequency_levels as (
-    select
-        invoice_count,
-        dense_rank() over (order by invoice_count desc) as freq_rank,
-
-        -- final frequency score from 1 to 5
-        ceiling(6 - dense_rank() over (order by invoice_count desc)) as frequency_score
-    from rfm
-    group by invoice_count
-),
-
-customer_segemnt_score as (
-	select
-		rfm.TraineeID,
-		rfm.BranchID,
-		rfm.Recency,
-		rfm.Invoice_Count,
-		rfm.Total_Price,
-		NTILE(5) over (order by Recency asc) as 'Recency_Score',
-		fl.Frequency_Score,
-		NTILE(5) over (order by Total_Price desc) as 'Monetary_Score'
-	from rfm
-	join frequency_levels fl
-	on rfm.Invoice_Count = fl.Invoice_Count
-)
-
-select css.TraineeID,
-	   css.BranchID,
-	   b.location,
-	   css.Recency_Score,
-	   css.Frequency_Score,
-	   css.Monetary_Score,
-	   (css.Recency_Score + css.Frequency_Score + css.Monetary_Score) as 'Total_Score',
-	    case
-			when (css.Recency_Score + css.Frequency_Score + css.Monetary_Score) >= 13 then 'Elite Members'
-			when (css.Recency_Score + css.Frequency_Score + css.Monetary_Score) >= 11 then 'Highly Active'
-			when (css.Recency_Score + css.Frequency_Score + css.Monetary_Score) >= 8 then 'Consistent Members'
-			when (css.Recency_Score + css.Frequency_Score + css.Monetary_Score) >= 6 then 'At-Risk Members'
-			when (css.Recency_Score + css.Frequency_Score + css.Monetary_Score) >= 4 then 'Inactive / Hibernating'
-			else 'Lost Members'
-		end as Member_Segment
-from customer_segemnt_score css
-join Branch b 
-on css.BranchID = b.ID
---order by Total_Score desc
-end
-
-go
+-- Financials
 
 create or alter proc Financials @Branch nvarchar(max)
 as
@@ -165,6 +85,26 @@ on b.ID = p.BranchID
 where b.ID in (SELECT CAST(value AS INT) 
         FROM STRING_SPLIT(@Branch, ','))
 group by b.location, YEAR([date])
+end
+
+go
+
+-- Feedback
+
+create or alter proc Branch_Feedback @Branch nvarchar(max)
+as
+begin
+select f.description, b.Location, count(tf.FeedbackID) as 'Feedback Count', YEAR([date]) AS [year]
+from branch b 
+join Trainee t
+on b.ID = t.BranchID
+join TraineeFeedback tf
+on t.ID = tf.TraineeID
+join Feedback f
+on tf.FeedbackID = f.ID
+where b.ID in (SELECT CAST(value AS INT) 
+        FROM STRING_SPLIT(@Branch, ','))
+group by b.Location, f.Description, YEAR([date]) 
 end
 
 
